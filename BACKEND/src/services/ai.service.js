3ad28f7dcf81c2927a2bf89a9d8f6c9b0ed4941b
ArpanSankesh <arpansankesh830@gmail.com>
2025-04-10 01:23:19 +0530

    API

--- BACKEND/src/services/ai.service.js
+++ BACKEND/src/services/ai.service.js
@@ -1,4 +1,4 @@
-const {GoogleGenerativeAI} = require('@google-generative-ai');
+const {GoogleGenerativeAI} = require('@google/genai');
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY                                                          );
 const model = genAI.model({model :'gemini-2.0-flash'});
